5bfa7a026ed144b23db3e22fd792a04cd3d059e4
Sara Nurestani <saranurestani@gmail.com>
2022-08-22 00:19:21 +0100

    Create transfer amount box

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
 
-
 function App() {
-  return (
+  const balance = 100;
 
+  return (
+    <div>
+      <label htmlFor="value" className="mx-auto flex flex-col gap-y-1">
+        Transfer Amount:
+      </label>
+      <input
+        type="number"
+        placeholder="Enter Amount"
+        className="rounded border pl-2"
+      />
+    </div>
   );
 }
-
 export default App;
