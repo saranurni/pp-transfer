f18fae176faca892df2e2579e1a4d77eeb9b68e1
Sara Nurestani <saranurestani@gmail.com>
2022-08-22 12:25:16 +0100

    Create button

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,49 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const balance = 100;
+  const [message, setMessage] = useState("");
+  const [balance, setBalance] = useState(100);
+  const [message, setMessage] = useState("");
+
+  function handleClick() {
+    if (amount <= balance) {
+      setBalance(balance - amount);
+      setMessage(`£${amount} has been transferred!`);
+    } else {
+      setMessage(`Insufficient funds.`);
+    }
+  }
+
+  const handleChange = (event) => setAmount(event.target.value);
 
   return (
-    <div>
-      <label htmlFor="value" className="mx-auto flex flex-col gap-y-1">
-        Transfer Amount:
-      </label>
-      <input
-        type="number"
-        placeholder="Enter Amount"
-        className="rounded border pl-2"
-      />
-    </div>
+    <form>
+      <div className="mx-auto flex flex-col gap-y-1 font-semibold">
+        <h1 className"mx-auto text center font-semibold">£{balance}</h1>
+        <label
+          htmlFor="value"
+          className="mx-auto flex flex-col gap-y-1 text-3xl text-pink-500 "
+          onChange={handleChange}
+        >
+          Transfer Amount:
+        </label>
+        <input
+          type="number"
+          placeholder="Enter Amount"
+          className="rounded border pl-4"
+        />
+      </div>
+
+      <button
+        type="submit"
+        value="Transfer"
+        className="text-pink border-pink mx-auto flex flex-col gap-y-1 rounded border px-4 py-2 text-center  text-xl text-pink-500"
+        onCLick={handleClick}
+      >
+        Transfer now
+      </button>
+    </form>
   );
 }
 export default App;
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,28 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import renderer from "react-test-renderer"
+import userEvent from "@testing-library/user-event"
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it ("renders without crashing", () => {
+  const rendered = renderer.create(<App />.toJSON();
+  expect(rendered).toMatchSnapshot();
+  )
+}):
+
+it("displays 'Insufficient funds' when transfer value doesn't meet requirement"), () => {
+  render (<App /> );
+
+  const balance = screen.getByLabelText(/balance/i);
+  const input = screen.getByAltText(/input/i);
+  const button = screen.getByRole(/button/i);
+
+  userEvent.type(input, 150)
+  userEvent.click(button)
+
+  expect(setMessage). toBe("Insufficient funds")
+
+}
+
+
+
+
