3d5d9e90ec552a0e1ff8707c1d4f3c125380d62b
Sara Nurestani <saranurestani@gmail.com>
2022-08-23 12:11:00 +0100

    Fix errors/typos

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-it("displays not enough funds when transfer value doesn't meet requirement", () => {
+it("displays Not enough funds. when transfer value doesn't meet requirement", () => {
   const balance = 100;
   const transferAmount = 150;
   const expectedTransferMsg = "Not enough funds.";
@@ -10,7 +10,7 @@ it("displays not enough funds when transfer value doesn't meet requirement", () 
 
   render(<App />);
   const input = screen.getByLabelText(/transfer amount:/i);
-  userEvent.type(input, transferAmount);
+  userEvent.type(input, transferAmount.toString());
   userEvent.click(screen.getByRole("button"));
 
   expect(screen.getByText(expectedBalanceMsg)).toBeInTheDocument();
@@ -19,12 +19,12 @@ it("displays not enough funds when transfer value doesn't meet requirement", () 
 
 it("prevents negative balances from occurring", () => {
   const transferAmount = 1000;
-  const expectedMsg = "You can't transfer more than ${balance}";
+  const expectedMsg = "Not enough funds.";
 
   render(<App />);
-  const input = screen.getByLabelText(/input/i);
-  userEvent.type(input, transferAmount);
+  const input = screen.getByLabelText(/transfer amount/i);
+  userEvent.type(input, transferAmount.toString());
   userEvent.click(screen.getByRole("button"));
 
-  expect(screen.getByText(expectedMsg)).toBe();
+  expect(screen.getByText(expectedMsg)).toBeInTheDocument();
 });
