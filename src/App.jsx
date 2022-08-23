0eaf747a90b7696a9073b938e0f5e1f0ed6c9997
Sara Nurestani <saranurestani@gmail.com>
2022-08-23 13:58:03 +0100

    Remove takeaway amount x2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form/Form";
 
-const balance = 100;
+let balance = 100;
 
 function App() {
   const [transferStatus, setTransferStatus] = useState({ amount: 0, msg: "" });
@@ -13,6 +13,7 @@ function App() {
     const transferAmount = event.target.elements[0].value;
 
     if (transferAmount < balance) {
+      balance -= transferAmount;
       setTransferStatus({
         amount: transferAmount,
         msg: "You transferred",
@@ -30,7 +31,7 @@ function App() {
     <>
       <Form handleSubmit={handleSubmit} />
       <p>You have successfully transferred {transferStatus.amount}</p>
-      <p>Current Balance:{balance - transferStatus.amount}</p>
+      <p>Current Balance:{balance}</p>
       <p>{transferStatus.msg}</p>
     </>
   );
